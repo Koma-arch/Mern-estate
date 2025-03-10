b232f58680bc58c8ae7be890abdc9866bf616fbf
Koma-arch <moonzarin161004@gmail.com>
2025-03-10 11:51:17 +0530

    Create Pages and Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,19 @@
-import React from 'react';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Home from './pages/Home';
+import SignIN from './pages/SignIN';
+import SignUp from './pages/Signup';
+import About from './pages/About';
+import Profile from './pages/Profile';
 
 export default function App(){
-  return <h1 className = 'text-red-500'>App</h1>
+  return <BrowserRouter>
+  <Routes>
+    <Route path = "/" element={<Home/>} />
+    <Route path = "/sign-in" element={<SignIN/>} />
+    <Route path = "/sign-up" element={<SignUp/>} />
+    <Route path = "/about" element={<About/>} />
+    <Route path = "/profile" element={<Profile/>} />
+
+  </Routes>
+  </BrowserRouter>;
 }
